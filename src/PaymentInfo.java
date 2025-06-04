4645933692882479e69d1076edaa44b661014836
Tuguldur72 <ttuk0650@gmail.com>
2025-06-04 13:54:57 +0800

    DeliveryOrder, PaymentInfo

--- src/PaymentInfo.java
+++ src/PaymentInfo.java
@@ -1,8 +1,27 @@
-
 public class PaymentInfo {
+    private double volume;      // Овор хэмжээ (жишээ нь, м3)
+    private double distance;    // Зай (жишээ нь, км)
+    private String mode;        // Горим: "Хурдан", "Тайван"
+    private double cost;        // Төлбөр
 
-    public PaymentInfo(Object volume, double d, String string) {
-        // TODO Auto-generated constructor stub
+    public PaymentInfo(double volume, double distance, String mode) {
+        this.volume = volume;
+        this.distance = distance;
+        this.mode = mode;
+        this.cost = calculateCost();
     }
 
+    private double calculateCost() {
+        double baseRate = mode.equalsIgnoreCase("Хурдан") ? 1500 : 1000;
+        return volume * 500 + distance * baseRate;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Төлбөр: " + cost + "₮ (Горим: " + mode + ")";
+    }
 }
