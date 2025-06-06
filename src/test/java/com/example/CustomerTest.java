a6342ea2dbf3f08449c48001f07638c3261a1f2e
Tsendsvren <tsekvv2006@gmail.com>
2025-06-06 14:54:27 +0800

    zaswar hiiv

--- src/test/java/com/example/CustomerTest.java
+++ src/test/java/com/example/CustomerTest.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 public class CustomerTest {
--- src/test/java/com/example/DeliveryTest.java
+++ src/test/java/com/example/DeliveryTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
